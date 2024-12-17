f942f773e831aefc8b1bcd38cc7c5e5f89f3849c
zakuma1 <47258376+zakuma1@users.noreply.github.com>
2024-12-18 01:54:07 +0200

    finished the todo list

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TodoList from './TodoList.jsx'
+const deigma = [
+  { id: 1, text: "psonia", isActive: false },
+  { id: 2, text: "douleia", isActive: false },
+  { id: 3, text: "programmatismo", isActive: true },
+  { id: 4, text: "sunenteuksh", isActive: false },
+  { id: 5, text: "douleia", isActive: false }
+]
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <TodoList arr={deigma} />
     </>
   )
 }
